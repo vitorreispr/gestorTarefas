8f485e766e35158497bdc92867d85c3858f67ab6
vitorreispr24@gmail.com <vitorreispr24@gmail.com>
2026-08-19 11:05:12 -0300

    funcionalidades componente lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { TaskDetails } from './task-details/task-details';
 
 export const routes: Routes = [
     {path: 'taskList', title: 'Lista de Tarefas', component: TaskList},
-    {path: 'taskDetails', title: 'Detalhes da Tarefa', component: TaskDetails},
+    {path: 'taskDetails/:id', title: 'Detalhes da Tarefa', component: TaskDetails},
     {path: '', redirectTo: 'taskList', pathMatch: 'full' }
 ];
--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TaskFilterComponent } from "../task-filter/task-filter";
+import { TaskService } from '../services/task';
+import { Task } from '../models/task';
+import { NgClass, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskFilterComponent],
+  imports: [TaskFilterComponent, CommonModule, FormsModule, RouterLink, NgClass],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
-export class TaskList {}
+export class TaskList {
+
+  construtor() {}
+
+  private taskService = inject(TaskService);
+
+  tasks: Array<Task> = [];
+
+  newTask = new Task();
+
+  ngOnInit() {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  addTask() {
+
+    this.taskService.addTask(this.newTask);
+
+    this.newTask = new Task();
+  }
+
+  removeTask(task: Task) {
+    this.taskService.removeTask(task);
+  }
+
+  uppdateTask() {
+    this.taskService.uppdateTask();
+  }
+
+  filterTasks(filter: string) {
+
+    if (filter !== '') {
+      this.tasks = this.tasks.filter(c => c.name.includes(filter))
+    }
+    else {
+      this.tasks = this.taskService.getTasks();
+    }
+  }
+}
