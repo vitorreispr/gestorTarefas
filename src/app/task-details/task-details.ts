7149de70ae151a83aa3de2541ef252dcd02bd221
vitorreispr24@gmail.com <vitorreispr24@gmail.com>
2026-08-19 16:51:20 -0300

    funcionalidades dos componentes

--- src/app/task-details/task-details.ts
+++ src/app/task-details/task-details.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from '../models/task';
+import { TaskService } from '../services/task';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.html',
   styleUrl: './task-details.css',
 })
-export class TaskDetails {}
+export class TaskDetails {
+
+  task?: Task;
+
+  constructor() {}
+
+  private route = inject(ActivatedRoute);
+
+  private taskService = inject(TaskService);
+
+  private router = inject(Router);
+
+  ngOnInit() {
+
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if (id === null) {
+      this.navigateBack();
+    }
+    else 
+      {
+        this.task = this.taskService.getById(+id);
+
+        if (this.task === undefined) {
+          this.navigateBack();
+        }
+    }
+    
+  }
+ private navigateBack() {
+    this.router.navigate(['/taskList'], {relativeTo: this.route});
+  }
+
+  save() {this.taskService.uppdateTask(); this.navigateBack();}
+
+  cancel() {this.navigateBack();}
+}
+
--- src/app/task-filter/task-filter.ts
+++ src/app/task-filter/task-filter.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-filter.html',
   styleUrl: './task-filter.css',
 })
-export class TaskFilterComponent {}
+export class TaskFilterComponent {
+
+  filter: string = '';
+
+  @Output() taskFilter = new EventEmitter<string>();
+
+  filterTask() {this.taskFilter.emit(this.filter)};
+}
